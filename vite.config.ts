4aba162ed562012e0b22312c9331c800a460d0c3
espinafr <theo.ruela@gmail.com>
2026-08-06 10:06:08 -0300

    Adiciona suporte para multi-página em produção

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
+import { globSync } from 'glob'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
   base: '/OpenTruco-client/',
+  build: {
+    rolldownOptions: {
+      input: Object.fromEntries(
+        globSync('*.html').map(file => [
+          path.basename(file, '.html'),
+          path.resolve(__dirname, file)
+        ])
+      )
+    }
+  }
 })
